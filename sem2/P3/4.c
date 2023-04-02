9e024d0cbdb828c4bda0b852d3037025a8694eec
Aditya Singh <asfairtexas@gmail.com>
2023-04-03 02:40:56 +0530

    String concat

--- sem2/P3/4.c
+++ sem2/P3/4.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
 #include <string.h>
 
-void concatN(char dest[], char src[], int n);
-
-int main() {
-    char dest[100] = "Hello";
-    char src[100] = "World";
-    int n = 3;
-    
-    concatN(dest, src, n);
-    printf("Result: %s\n", dest);
-    
-    return 0;
+void concat(char str1[], char str2[], int n) {
+    strncat(str1, str2, n); // concatenate first n characters of str1 to str2
 }
 
-void concatN(char dest[], char src[], int n) {
-    strncat(dest, src, n);
+int main() {
+    char str1[100], str2[100];
+    int n;
+
+    printf("Enter first string: ");
+    scanf("%[^\n]s", str1);
+    getchar(); // consume newline character
+
+    printf("Enter second string: ");
+    scanf("%[^\n]s", str2);
+    getchar(); // consume newline character
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    concat(str1, str2, n);
+
+    printf("Concatenated string: %s\n", str1);
+
+    return 0;
 }
